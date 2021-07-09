2579e5b5eb23a84a4f014cde8cf094ad557ef37b
sophie <sophiefishman@gmail.com>
2021-07-09 12:47:53 -0500

    adds search bar to movies page

--- src/views/Movies/Index.vue
+++ src/views/Movies/Index.vue
@@ -2,10 +2,55 @@
   <div class="movies">
     <h1>Movies</h1>
     <div>
-      <form v-on:submit.prevent="searchMovies()">
+      <!-- <form v-on:submit.prevent="searchMovies()">
         <input type="text" v-model="search" placeholder="search movies" />
         <input type="submit" value="Search" />
-      </form>
+      </form> -->
+      <section class="mt-4 pt-2">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 my-3">
+              <aside class="search-box mb-4 pb-2 fs-14">
+                <form class="position-relative" v-on:submit.prevent="searchMovies()">
+                  <input
+                    class="form-control position-relative bg-light border rounded-pill"
+                    v-model="search"
+                    placeholder="Search..."
+                  />
+                  <button
+                    class="
+                      search-button
+                      border-0
+                      bg-transparent
+                      position-absolute
+                      top-50
+                      end-0
+                      translate-middle-y
+                      fs-20
+                      me-2
+                    "
+                    type="submit"
+                  >
+                    <span data-feather="search" class="icon-xs text-muted"></span>
+                  </button>
+                </form>
+              </aside>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="row">
+                <div class="col-lg-6"></div>
+                <div class="col-lg-6"></div>
+                <div class="col-lg-6"></div>
+                <div class="col-lg-6"></div>
+              </div>
+              <!-- end row -->
+            </div>
+          </div>
+        </div>
+        <!-- end container -->
+      </section>
     </div>
     <br />
     <div v-for="movie in movies" v-bind:key="movie.id">
